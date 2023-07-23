c2568a8e10f4c4c26bc0fde9a1e98f8d50427255
editor-taka <takachan1050150@gmail.com>
2023-07-23 10:56:22 -0400

    promise practice

---
+++ mdn/promise.js
@@ -0,0 +1,14 @@
+const fetchPromise = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
+
+fetchPromise
+    .then((res) =>{
+        if(!res.ok){
+            throw new Error(`HTTP error: ${res.status}`);
+        }
+        return res.json();
+    })
+    .then((data) =>{
+        data.forEach(element => {
+            console.log(element.name);
+        });
+    })
