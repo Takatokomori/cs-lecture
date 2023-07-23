d90dfb195d60ac8a4a84b74a7935695e162751e4
editor-taka <takachan1050150@gmail.com>
2023-07-23 11:39:29 -0400

    promise all

--- mdn/promise.js
+++ mdn/promise.js
@@ -11,7 +11,23 @@ fetchPromise
         data.forEach(element => {
             console.log(element.name);
         });
+        console.log("Done");
     })
     .catch((error)=>{
         console.log(`Couldn't get products: ${error}`);
     })
+
+    const fetchPromise1 = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
+    const fetchPromise2 = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found');
+    // const fetchPromise3 = fetch('https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json');
+    const fetchPromise3 = fetch('bad-scheme://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json');
+
+Promise.all([fetchPromise1, fetchPromise2, fetchPromise3])
+  .then((responses) => {
+    for (const response of responses) {
+      console.log(`${response.url}: ${response.status}`);
+    }
+  })
+  .catch((error) => {
+    console.error(`Failed to fetch: ${error}`)
+});
